073d48c54dbe782747341b3d4f73d179ad20ffd8
Phindie <pn8256@gmail.com>
2021-03-24 14:14:42 +0200

    unstage changes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { v4 as createId } from "uuid";
 import { BrowserRouter, Switch, Route, useParams } from "react-router-dom";
+import identity from "netlify-identity-widget";
 
 import Home from "../../views/Home/Home";
 import Add from "../../views/Add/Add";
@@ -9,44 +10,42 @@ import Edit from "../../views/Edit/Edit";
 const EditWrapper = (props) => {
   const { list, ...remainingProps } = props;
   const { taskId } = useParams();
-  const { name } = list.find((item) => item.id === taskId)
-  return <Edit {...remainingProps} taskId={taskId} initialName={name} />
-}; 
+
+  if (list.length < 1) {
+    return <div>Loading...</div>;
+  }
+
+  const { name } = list.find((item) => item.id === taskId);
+  return <Edit {...remainingProps} taskId={taskId} initialName={name} />;
+};
 
 const App = () => {
   const [loaded, setLoaded] = useState(false);
   const [list, setList] = useState([]);
 
-  useEffect(
-    () => {
-     const listAsString = window.localStorage.getItem('list')
+  useEffect(() => {
+    const listAsString = window.localStorage.getItem("list");
 
-     if(listAsString) {
-       setList(JSON.parse(listAsString))
-     }
-     setLoaded(true);
+    if (listAsString) {
+      setList(JSON.parse(listAsString));
+    }
+    setLoaded(true);
 
-    },
-    []
-  )
+    identity.init();
+    const userObj = identity.currentUser();
 
-  useEffect(
-    () => {
-      if(loaded){
-       window.localStorage.setItem(
-      'list',
-      JSON.stringify(list),
-        )
-       }
-      },
-    [list, loaded],
-  )
+    if (!userObj) {
+      identity.open();
+    }
+  }, []);
+
+  useEffect(() => {
+      window.localStorage.setItem("list", JSON.stringify(list));
+    }
+  }, [list, loaded]);
 
   const handleAddItem = (name) => {
-    setList([
-      { id: createId(), name, checked: false }, 
-      ...list,
-    ])
+    setList([{ id: createId(), name, checked: false }, ...list]);
     window.location.replace("/");
   };
 
@@ -82,25 +81,48 @@ const App = () => {
     window.location.replace("/");
   };
 
+
+
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/edit/:taskId">
-         <EditWrapper list={list} onSave={handleEditItem} />
-         </Route>
-      
-          
-        <Route path="/add/">
-        <Add onSave={handleAddItem} /> 
-        </Route>
+        <Route
+          path="/edit/:taskId"
+          children={
+            <EditWrapper
+              list={list}
+              onSave={handleEditItem}
+              userName={null}
+              onLogin={console.log}
+              onUserClick={console.log}
+            />
+          }
+        />
+        <Route
+          path="/add/"
+          children={
+            <Add
+              onSave={handleAddItem}
+              userName={null}
+              onLogin={console.log}
+              onUserClick={console.log}
+            />
+          }
+        />
 
-          <Route path="/">
-             <Home
+        <Route path="/">
+          children=
+          {
+            <Home
               list={list}
               onCheckToggle={handleCheckToggle}
               onDeleteItem={handleDeleteItem}
+              userName={null}
+              onLogin={console.log}
+              onUserClick={console.log}
             />
-            </Route>
+          }
+        </Route>
       </Switch>
     </BrowserRouter>
   );
